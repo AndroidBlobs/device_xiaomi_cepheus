92c7e97e1f90cbf4d01f0bf597d8048196d040a2
ShivamKumarJha <jha.shivam3@gmail.com>
2020-02-10 20:28:44 +0000

    cepheus-user-10-QKQ1.190825.002-V11.0.7.0.QFAEUXM-release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- lineage_cepheus.mk
+++ lineage_cepheus.mk
@@ -17,7 +17,7 @@ PRODUCT_MODEL := MI 9
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := cepheus
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="cepheus-user 10 QKQ1.190825.002 9.12.5 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="cepheus-user 10 QKQ1.190825.002 V11.0.7.0.QFAEUXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/cepheus/cepheus:10/QKQ1.190825.002/9.12.5:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/cepheus_eea/cepheus:10/QKQ1.190825.002/V11.0.7.0.QFAEUXM:user/release-keys
